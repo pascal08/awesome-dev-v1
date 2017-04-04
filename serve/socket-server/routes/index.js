a7f3e4b301abf7b4427e519a9ac30dd8e9802f7b
Jeffrey Arts <jeffrey@wearereasonablepeople.com>
2017-04-04 09:25:07 +0200

    Add support for middleware

--- serve/socket-server/routes/index.js
+++ serve/socket-server/routes/index.js
@@ -10,8 +10,8 @@ module.exports = (req, res) => {
     User.init(req, res);
     Room.join(req, res, "general");
 
-    route.add("user.update", User.update)
-        .add("room.join", Room.join)
+    route.add("user.update", function(req,res, next){ console.log('Middleware test');next();}, User.update)
+        .add("room.join",Room.join)
         .add("room.leave", Room.leave)
         .add("disconnect", Room.leave)
 
--- serve/socket-server/utilities/socket-route.js
+++ serve/socket-server/utilities/socket-route.js
@@ -6,10 +6,22 @@ module.exports = (req, res) => {
     return {
         req: req,
         res: res,
-        add: function(routeName, fn) {
+        add: function(routeName, ...args) {
+            let index = 0;
+            const next = () => {
+                if (typeof args[index] !== "function") {
+                    return;
+                }
+                index++;
+                args[index-1](this.req, this.res, next);
+            };
+
+
+            // console.log(this.req.on);
             this.req.on( routeName, value => {
                 this.req.body = value;
-                fn(this.req, this.res);
+                index = 0;
+                next();
             });
             return this;
         }
