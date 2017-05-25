503a1eedde3777644688b9e40a2085a2d3766021
JeffreyArts <sjeffff@gmail.com>
2017-05-25 20:24:59 +0200

    Add security so only selected value can be within the accesstoken (_id, email & name)

--- serve/api-server/controllers/auth/access-token.js
+++ serve/api-server/controllers/auth/access-token.js
@@ -1,14 +1,18 @@
 /* global requireShared */
 
 const signToken = requireShared("utilities/signToken");
+const {pick} = require("lodash");
 
 
 module.exports = (req, res) => {
+
+    const user = pick(req.user, ["_id", "email", "name"]);
+
     if (req.session.redirectUrl) {
-        const url = `${req.session.redirectUrl}?accessToken=${signToken(req.user)}`;
+        const url = `${req.session.redirectUrl}?accessToken=${signToken(user)}`;
         res.redirect(url);
     } else {
-        res.status(202).send({accessToken: signToken(req.user)});
+        res.status(202).send({accessToken: signToken(user)});
     }
 
     delete req.session.redirectUrl;
