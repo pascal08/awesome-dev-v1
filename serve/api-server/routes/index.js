df682ceb7b0e56a5928d3b85ee413d7d8df93a52
JeffreyArts <sjeffff@gmail.com>
2017-05-26 15:06:48 +0200

    Made routing file look a bit cleaner

--- serve/api-server/routes/index.js
+++ serve/api-server/routes/index.js
@@ -5,29 +5,29 @@ const localAuthorize = requireApi("passport-strategies/local").authorize;
 const facebookAuthorize = requireApi("passport-strategies/facebook").authorize;
 const googleAuthorize = requireApi("passport-strategies/google").authorize;
 
-const returnAccessToken = requireApi("controllers/auth/access-token");
+// Middleware
+const setReturnUrl = requireApi("middleware/auth/set-return-url")
 
 
 module.exports = function(app) {
-
-  // Home
-    app.get("/", requireApi("controllers/home"));
-
-  // Accounts / Authorization
-    app.delete("/accounts/:accountId", requireApi("controllers/account/delete"));
-    app.post("/register", requireApi("controllers/account/create"));
-    app.get("/auth", isAuthorized, requireApi("controllers/auth/me"));
-
+    // Home
+    app.get("/"                                                                     , requireApi("controllers/home"));
+    // Accounts / Authorization
+    app.delete("/accounts/:accountId"                                               , requireApi("controllers/account/delete"));
+    app.post("/accounts"                                                            , requireApi("controllers/account/create"));
+    app.post("/register"                                                            , requireApi("controllers/account/create"));
+    app.get("/auth"                     , isAuthorized                              , requireApi("controllers/auth/me"));
     // Facebook auth
-    app.get("/auth/facebook", requireApi("middleware/auth/set-return-url"), facebookAuthorize);
-    app.get("/auth/facebook/callback", facebookAuthorize, returnAccessToken);
-
+    app.get("/auth/facebook"            , setReturnUrl , facebookAuthorize);
+    app.get("/auth/facebook/callback"   , facebookAuthorize                         , requireApi("controllers/auth/access-token"));
     // Google auth
-    app.get("/auth/google", requireApi("middleware/auth/set-return-url"), googleAuthorize);
-    app.get("/auth/google/callback", googleAuthorize, returnAccessToken);
-
+    app.get("/auth/google"              , setReturnUrl, googleAuthorize);
+    app.get("/auth/google/callback"     , googleAuthorize                           , requireApi("controllers/auth/access-token"));
     // Local auth
-    app.post("/auth", localAuthorize, returnAccessToken);
+    app.post("/auth"                    , localAuthorize                            , requireApi("controllers/auth/access-token"));
+
+
+
 
     app.get("/test", isAuthorized, (req,res) => {
         res.status(200).send(req.user);
