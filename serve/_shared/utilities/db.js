55d37b9954322a16b622375f3e2b58cd373c570d
J <sjeffff@gmail.com>
2017-06-02 14:14:51 +0200

    Add error message when db error occurs (#10)

--- serve/_shared/utilities/db.js
+++ serve/_shared/utilities/db.js
@@ -1,4 +1,8 @@
 const Config = require("config"),
     monk = require("monk");
 
-module.exports = monk(Config.mongodb.database);
+module.exports = monk(Config.mongodb.database, err => {
+    if(err){
+        console.error("Not connected \r\n", err.message);
+    }
+});
